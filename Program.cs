e6fc224c4251ea58596a3ab56cdc1cdc3bf2a721
Harshil Prasad <ac122323@avcol.school.nz>
2023-03-15 11:49:01 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿String Maori;
+int name; 
+
+
+Console.WriteLine("Welcome to my Te Reo Maori Quiz");
+Console.WriteLine("There'll be 4 questions per level ");
+Console.WriteLine("please enter your name: ");
+Console.ReadLine(); 
+
+
+
