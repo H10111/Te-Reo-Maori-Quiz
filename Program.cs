61f1e1925f7c37c6061852d1a5fe441e94ba92f8
Harshil Prasad <ac122323@avcol.school.nz>
2023-03-23 10:46:43 +1300

    easy level works

--- Program.cs
+++ Program.cs
@@ -1,42 +1,60 @@
 ﻿String inputstr,name;
-int input; 
- 
+int input;
+
 
 Console.WriteLine("\t\t\t\t\tWelcome to my Te Reo Maori Quiz");
 Console.WriteLine("\nThere's 4 questions per level of difficulty ");
-Console.WriteLine("\nplease enter your name: ");
-name = Console.ReadLine(); 
-Console.WriteLine("\nWell hello there "+name+", I welcome you to my quiz");
-Console.WriteLine(value: $"\nplease select a level \n(1)Easy \n(2)Intermidiate\n(3)Expert\n(4)Exit");
-input = Convert.ToInt32(Console.ReadLine()); 
-inputstr=Convert.ToString((input));
-if (input == 1)
-{
-    Easy();
-}
-else if (input == 2)
-{
-    Intermidiate();
-}
-else if (input == 3)
-{
-    Expert(); 
-}
-else if (input ==4)
-{
-    Exit(); 
-}
-
+Console.WriteLine("\nplease enter your name: ");//gets users name
+name = Console.ReadLine(); //stores
+Console.WriteLine("\nWell hello there "+name+", I welcome you to my quiz");//welcomes user
+Console.WriteLine(value: $"\nplease select a level \n(1)Easy \n(2)Intermidiate\n(3)Expert\n(4)Exit");//selects level
+input = Convert.ToInt32(Console.ReadLine());
+inputstr = Convert.ToString((input));
+while (true)//while loop starts
 {
 
-}
 
-Console.Read();
+    if (input < 1 || input > 4)
+    {
+        Console.WriteLine("please choose the numbers listed 1,2,3,4");
+    }
+    else if (input == 1)
+    {
+        Easy();
+    }
+    else if (input == 2)
+    {
+        Intermidiate();
+    }
+    else if (input == 3)
+    {
+        Expert();
+    }
+    else if (input == 4)
+    {
+        Exit();
+    }
+    break; 
+}//while loop ends
+
+
+
 
 
 static void Easy()
 {//added levels with greeting 
+    Console.Clear(); 
+    int answer1;
     System.Console.WriteLine("You have chosen easy mode");
+    
+    Console.WriteLine("lets start with the first question\nWhat is hello in Maori");
+    
+    Console.WriteLine($"select your answer\n(a)Kia ora\n(b)Tena koutou\n(c)Whanau");
+    Console.ReadLine();
+    
+    
+    
+
 }
 static void Intermidiate()
 {
